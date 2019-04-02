a3d2e86ba21abbb7bb600b4550ad51a8d7dd580d
Adam Bien <abien@adam-bien.com>
2019-04-02 14:33:08 +0200

    alternative deactivates impls

--- hello/src/main/java/com/airhacks/ping/boundary/NotificationImpl.java
+++ hello/src/main/java/com/airhacks/ping/boundary/NotificationImpl.java
@@ -1,10 +1,13 @@
 
 package com.airhacks.ping.boundary;
 
+import javax.enterprise.inject.Alternative;
+
 /**
  *
  * @author airhacks.com
  */
+@Alternative
 public class NotificationImpl implements Notification {
 
     @Override
