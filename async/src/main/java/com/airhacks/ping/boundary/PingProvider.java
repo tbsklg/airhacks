c31ca4bb078cbcf242eb259e9e49034f918c4f15
Adam Bien <abien@adam-bien.com>
2019-04-02 16:46:20 +0200

    reactive programming -> don't do it at home

--- async/src/main/java/com/airhacks/ping/boundary/PingProvider.java
+++ async/src/main/java/com/airhacks/ping/boundary/PingProvider.java
@@ -1,14 +1,38 @@
 
 package com.airhacks.ping.boundary;
 
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
 /**
  *
  * @author airhacks.com
  */
 public class PingProvider {
 
-    public String nextPing() {
-        return "ping " + System.currentTimeMillis();
+    @Resource
+    ManagedExecutorService clientService;
+    private Client client;
+
+    @PostConstruct
+    public void init() {
+        this.client = ClientBuilder.newBuilder().
+                connectTimeout(2, TimeUnit.SECONDS).
+                readTimeout(2, TimeUnit.SECONDS).
+                executorService(clientService).build();
+    }
+
+
+    public CompletionStage<String> nextPing() {
+        return this.client.target("http://airhacks.com").
+                request(MediaType.TEXT_PLAIN).rx().
+                get(String.class);
     }
 
 }
--- async/src/main/java/com/airhacks/ping/boundary/PingResource.java
+++ async/src/main/java/com/airhacks/ping/boundary/PingResource.java
@@ -1,9 +1,6 @@
 package com.airhacks.ping.boundary;
 
-import static java.util.concurrent.CompletableFuture.supplyAsync;
 import java.util.concurrent.TimeUnit;
-import javax.annotation.Resource;
-import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -20,15 +17,11 @@ public class PingResource {
     @Inject
     PingProvider provider;
 
-    @Resource
-    ManagedExecutorService mes;
-
     @GET
     public void ping(@Suspended AsyncResponse response) {
         response.setTimeout(2, TimeUnit.SECONDS);
-        supplyAsync(provider::nextPing, mes).
-                thenApply(this::transform).
-                thenAccept(response::resume);
+        provider.nextPing().thenApply(this::transform).thenAccept(response::resume);
+
     }
 
     String transform(String input) {
