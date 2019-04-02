192edfb60369977d729b406f9310b1bc83d1d52c
Adam Bien <abien@adam-bien.com>
2019-04-02 13:03:31 +0200

    Business metrics with counters implemented

--- hello/src/main/java/com/airhacks/ping/boundary/PingListener.java
+++ hello/src/main/java/com/airhacks/ping/boundary/PingListener.java
@@ -3,6 +3,9 @@ package com.airhacks.ping.boundary;
 
 import javax.enterprise.event.Observes;
 import javax.enterprise.event.TransactionPhase;
+import javax.inject.Inject;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import org.eclipse.microprofile.metrics.annotation.RegistryType;
 
 /**
  *
@@ -10,11 +13,17 @@ import javax.enterprise.event.TransactionPhase;
  */
 public class PingListener {
 
+    @Inject
+    @RegistryType(type = MetricRegistry.Type.APPLICATION)
+    MetricRegistry registry;
+
     public void onSuccessfulPing(@Observes(during = TransactionPhase.AFTER_SUCCESS) Ping ping) {
+        registry.counter("ping_created").inc();
         System.out.println("++ " + ping);
     }
 
     public void onFailedPing(@Observes(during = TransactionPhase.AFTER_FAILURE) Ping ping) {
+        registry.counter("ping_failed").inc();
         System.out.println("-- " + ping);
     }
 
